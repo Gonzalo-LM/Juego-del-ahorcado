a57fda2e7744f574b2e1a9ef2206a84fa3ace585
Gonzalo-LM <paraestudios.19@gmail.com>
2023-01-10 11:44:15 -0500

    reajustando todo el programa

--- js/ahorcado.js
+++ js/ahorcado.js
@@ -2,10 +2,17 @@ var ingresar_palabra = document.querySelector("#palabra");
 var ingresar_letra = document.querySelector("#letra");
 var adivinar_palabra = document.querySelector("#adivinar");
 var cantidad_palabra = document.querySelector("#cantidad");
+var ingresar_oracion = document.querySelector("#oracion");
 
 var btn_empezar = document.querySelector("#btnempezar");
-var btn_ingresar = document.querySelector("#btnIngresar");
+var btn_ingresar_letra = document.querySelector("#btnIngresarLetra");
+var btn_ingresar_oracion = document.querySelector("#btnIngresarOracion");
+var btn_cambiar_letra = document.querySelector("#btncambiarLetra");
+var btn_cambiar_oracion = document.querySelector("#btncambiarOracion");
 var btn_limpiar = document.querySelector("#limpiar");
+
+var div_letra = document.querySelector("#campo_letra");
+var div_oracion = document.querySelector("#campo_oracion");
 
 var cuerpo = document.querySelector("#cuerpo");
 var brazo_D = document.querySelector("#brazo_D");
@@ -19,8 +26,13 @@ brazo_I.style.visibility = "hidden";
 pierna_D.style.visibility = "hidden";
 pierna_I.style.visibility = "hidden";
 
+
+div_oracion.style.display = "none";
+div_letra.style.display = "block";
+
 ingresar_letra.disabled = true;
-btn_ingresar.disabled = true;
+btn_ingresar_letra.disabled = true;
+btn_cambiar_letra.disabled = true;
 
 ingresar_palabra.focus();
 
@@ -30,9 +42,33 @@ var palabra = "";
 var frase = "";
 var intentos = 5;
 
-function desactivar(){
-    ingresar_letra.disabled = true;
-    btn_ingresar.disabled = true;
+function activar_desactivar(){
+    if(palabra != adivinar_palabra.value){
+        ingresar_letra.disabled = false;
+        btn_ingresar_letra.disabled = false;
+        btn_cambiar_letra.disabled = false;
+    }else{
+        ingresar_letra.disabled = true;
+        btn_ingresar_letra.disabled = true;
+        btn_cambiar_letra.disabled = true;
+        ingresar_oracion.disabled = true;
+        btn_ingresar_oracion.disabled = true;
+        btn_cambiar_oracion.disabled = true;
+    }
+}
+
+function cambiarCampoLetra(){
+    div_oracion.style.display = "block";
+    div_letra.style.display = "none";
+    ingresar_oracion.value = "";
+    ingresar_oracion.focus();
+}
+
+function cambiarCampoOracion(){
+    div_oracion.style.display = "none";
+    div_letra.style.display = "block";
+    ingresar_letra.value = "";
+    ingresar_letra.focus();
 }
 
 function validar(letra){
@@ -66,7 +102,7 @@ function validar(letra){
         else if(intentos == 0){
             pierna_I.style.visibility = "visible";
             alert("Perdiste todos tus intentos");
-            desactivar();
+            activar_desactivar();
         }
         ingresar_letra.value = "";
         ingresar_letra.focus();
@@ -76,9 +112,7 @@ function validar(letra){
 
 function enviarPalabra(){
     if(ingresar_palabra.value != ""){
-        if(adivinar.length == 0){
-            ingresar_letra.disabled = false;
-            btn_ingresar.disabled = false;
+        if(adivinar.length == 0){      
             adivinar = ingresar_palabra.value.toLowerCase().split("");
             palabra = ingresar_palabra.value.toLowerCase();
             cantidad = palabra.replaceAll(" ", "");
@@ -91,6 +125,7 @@ function enviarPalabra(){
                 }
                 adivinar_palabra.value = oracion.toString().replaceAll(",", "");
             }
+            activar_desactivar();
             ingresar_palabra.value = "";
             ingresar_letra.focus();
         }else if( adivinar_palabra.value == palabra){
@@ -113,18 +148,42 @@ function enviarPalabra(){
 
 function adivinarPalabra(){
     let texto = ingresar_letra.value.toLowerCase();
-    if(adivinar.length != 0){
-        for(var i = 0; i < adivinar.length; i++){
-            validar(texto);
-            break;
+    if(texto != ""){
+        if(adivinar.length != 0){
+            for(var i = 0; i < adivinar.length; i++){
+                validar(texto);
+                break;
+            }
         }
+    }else{
+        alert("no has ingresado la letra");
+        ingresar_letra.focus();
     }
     if(palabra == adivinar_palabra.value){
         alert("Ganaste!!! lograste adivinar la palabra");
-        desactivar();
+        activar_desactivar();
+    }
+}
+
+function adivinarPalabraCompleta(){
+    let textocompleto = ingresar_oracion.value.toLowerCase();
+    if(textocompleto != ""){
+        if(palabra == textocompleto){
+            adivinar_palabra.value = textocompleto;
+            alert("Ganaste!!! lograste adivinar la palabra");
+            activar_desactivar();
+        }else{
+            validar(textocompleto);
+        }
+    }else{
+        alert("no has ingresado la oracion");
+        ingresar_oracion.focus();
     }
 }
 
 
 btn_empezar.onclick = enviarPalabra;
-btn_ingresar.onclick = adivinarPalabra;
+btn_ingresar_letra.onclick = adivinarPalabra;
+btn_ingresar_oracion.onclick = adivinarPalabraCompleta;
+btn_cambiar_letra.onclick = cambiarCampoLetra;
+btn_cambiar_oracion.onclick = cambiarCampoOracion;
